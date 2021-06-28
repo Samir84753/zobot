753bc7914b4b0a51b9ed83cbcbdd3e5bfd5a4e0f
Samir Maharjan <samir84753@gmail.com>
2021-06-28 10:30:08 +0545

    spelling correction

--- topcollaborator/cogs/urban.py
+++ topcollaborator/cogs/urban.py
@@ -8,7 +8,7 @@ class Urban_dictionary_commands(commands.Cog):
     def __init__(self,bot):
         self.bot=bot
         self.url='http://api.urbandictionary.com/v0/define?term='
-    @commands.command(name='urban',help=": urban defination")
+    @commands.command(name='urban',help=": urban definition")
     async def urban(self,ctx,*,search='urban'):
         session = aiohttp.ClientSession()
         await ctx.trigger_typing()
@@ -17,20 +17,20 @@ class Urban_dictionary_commands(commands.Cog):
             defn=requests.get(url).json()
 
             if len(defn['list'])>1:
-                defination_One=defn['list'][0]['definition']
+                definition_One=defn['list'][0]['definition']
                 example_One=defn['list'][0]['example']
-                defination_Two=defn['list'][1]['definition']
+                definition_Two=defn['list'][1]['definition']
                 example_Two=defn['list'][1]['example']
-                defination_Tnree=defn['list'][2]['definition']
+                definition_Tnree=defn['list'][2]['definition']
                 example_Three=defn['list'][2]['example']
 
-                embedmsg = discord.Embed(title="Urban Defination",colour=discord.Colour.blue())
+                embedmsg = discord.Embed(title="Urban Definition",colour=discord.Colour.blue())
                 embedmsg.add_field(name='Word',value=search,inline=False)
-                embedmsg.add_field(name='Defination 1',value=defination_One,inline=False)
+                embedmsg.add_field(name='Definition 1',value=definition_One,inline=False)
                 embedmsg.add_field(name='Example 1',value=example_One,inline=False)
-                embedmsg.add_field(name='Defination 2',value=defination_Two,inline=False)
+                embedmsg.add_field(name='Definition 2',value=definition_Two,inline=False)
                 embedmsg.add_field(name='Example 2',value=example_Two,inline=False)
-                embedmsg.add_field(name='Defination 3',value=defination_Tnree,inline=False)
+                embedmsg.add_field(name='Defintion 3',value=definition_Three,inline=False)
                 embedmsg.add_field(name='Example 3',value=example_Three,inline=False)
         
                 await ctx.send(embed=embedmsg)
