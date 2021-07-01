ba4eddc5a046faedfe295b7214197028cebc1192
Samir Maharjan <samir84753@gmail.com>
2021-07-01 10:31:03 +0545

    fix urban.py

--- topcollaborator/cogs/urban.py
+++ topcollaborator/cogs/urban.py
@@ -14,27 +14,33 @@ class Urban_dictionary_commands(commands.Cog):
         await ctx.trigger_typing()
         try:
             url=self.url+str(search)
-            print(url)
             defn=requests.get(url).json()
-
-            if len(defn['list'])>1:
+            
+            if len(defn['list'])>=2:
                 definition_One=defn['list'][0]['definition']
                 example_One=defn['list'][0]['example']
                 definition_Two=defn['list'][1]['definition']
                 example_Two=defn['list'][1]['example']
-                definition_Three=defn['list'][2]['definition']
-                example_Three=defn['list'][2]['example']
-
                 embedmsg = discord.Embed(title="Urban Definition",colour=discord.Colour.blue())
                 embedmsg.add_field(name='Word',value=search,inline=False)
                 embedmsg.add_field(name='Definition 1',value=definition_One,inline=False)
                 embedmsg.add_field(name='Example 1',value=example_One,inline=False)
                 embedmsg.add_field(name='Definition 2',value=definition_Two,inline=False)
                 embedmsg.add_field(name='Example 2',value=example_Two,inline=False)
-                embedmsg.add_field(name='Defintion 3',value=definition_Three,inline=False)
-                embedmsg.add_field(name='Example 3',value=example_Three,inline=False)
         
                 await ctx.send(embed=embedmsg)
+
+			elif len(defn['list'])==1:
+				definition_One=defn['list'][0]['definition']
+                example_One=defn['list'][0]['example']
+
+                embedmsg = discord.Embed(title="Urban Definition",colour=discord.Colour.blue())
+                embedmsg.add_field(name='Word',value=search,inline=False)
+                embedmsg.add_field(name='Definition',value=definition_One,inline=False)
+                embedmsg.add_field(name='Example',value=example_One,inline=False)
+        
+                await ctx.send(embed=embedmsg)
+			
             else:
                 await ctx.send('No result found.')
         except:
